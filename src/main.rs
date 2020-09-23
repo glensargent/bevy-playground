0b61809f088b0432e038917facb6b3b402d338e3
Glen Sargent <glensarg@hotmail.co.uk>
2020-09-23 12:06:17 +0100

    basic ui

--- src/main.rs
+++ src/main.rs
@@ -15,25 +15,7 @@ fn main() {
             ..Default::default()
         })
         .add_default_plugins()
-        .add_startup_system(setup.system())
+        .add_startup_system(systems::setup.system())
         .add_system(systems::player_movement_system.system())
         .run();
-}
-
-fn setup (
-    mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    _asset_server: Res<AssetServer>,
-) {
-    commands
-        .spawn(Camera2dComponents::default())
-        .spawn(UiCameraComponents::default())
-        // spawn player
-        .spawn(SpriteComponents {
-            material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
-            transform: Transform::from_translation(Vec3::new(0.0, -215.0, 0.0)),
-            sprite: Sprite::new(Vec2::new(30.0, 30.0)),
-            ..Default::default()
-        })
-        .with(components::Player { speed: 500.0 });
 }
--- src/systems.rs
+++ src/systems.rs
@@ -7,6 +7,54 @@ use bevy::{
 
 use crate::components;
 
+pub fn setup (
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    let font_handle = asset_server.load("assets/fonts/Roboto-Regular.ttf").unwrap();
+
+    commands
+        .spawn(Camera2dComponents::default())
+        .spawn(UiCameraComponents::default())
+        // build ui
+        .spawn(NodeComponents {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Px(50.0)),
+                border: Rect::all(Val::Px(2.0)),
+                ..Default::default()
+            },
+            material: materials.add(Color::rgb(0.0, 0.0, 0.0).into()),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            // text
+            parent.spawn(TextComponents {
+                style: Style {
+                    align_self: AlignSelf::FlexEnd,
+                    ..Default::default()
+                },
+                text: Text {
+                    value: "my hotbar:".to_string(),
+                    font: font_handle,
+                    style: TextStyle {
+                        font_size: 28.0,
+                        color: Color::WHITE,
+                    },
+                },
+                ..Default::default()
+            });
+        })
+        // spawn player
+        .spawn(SpriteComponents {
+            material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
+            transform: Transform::from_translation(Vec3::new(0.0, -215.0, 0.0)),
+            sprite: Sprite::new(Vec2::new(30.0, 30.0)),
+            ..Default::default()
+        })
+        .with(components::Player { speed: 500.0 });
+}
+
 pub fn player_movement_system(
     time: Res<Time>,
     keyboard_input: Res<Input<KeyCode>>,
